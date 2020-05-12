864b2f35e0df36b4285f583b5cf2431ce4d1ece1
Holecz Tamás <rasztamas79@gmail.com>
2020-05-12 13:23:01 +0200

    Fix profile type user namespace

--- Form/ProfileType.php
+++ Form/ProfileType.php
@@ -2,7 +2,7 @@
 
 namespace HT\UserBundle\Form;
 
-use HT\Entity\User;
+use HT\UserBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
