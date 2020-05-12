afdaa94dbd816835e0670a6b413d08ad84a1adf4
Holecz Tamás <rasztamas79@gmail.com>
2020-05-12 11:52:04 +0200

    Fix extension name

--- DependencyInjection/HTUserExtension.php
+++ DependencyInjection/HTUserExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class UserExtension extends Extension
+class HTUserExtension extends Extension
 {
 	public function load(array $configs, ContainerBuilder $container)
 	{
