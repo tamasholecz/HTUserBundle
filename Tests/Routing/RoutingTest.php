6bcf629fcbb1e19681f804c8aeef1f8da5c7b3bb
Holecz Tamás <rasztamas79@gmail.com>
2020-05-14 13:22:53 +0200

    Routing test

---
+++ Tests/Routing/RoutingTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace HT\UserBundle\Tests\Routing;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Yaml\Yaml;
+
+class RoutingTest extends TestCase
+{
+	/**
+	 * @dataProvider loadRoutingProvider
+	 */
+	public function testLoadRouting(string $routeName, string $path, array $methods)
+	{
+		$locator = new FileLocator();
+		$loader = new YamlFileLoader($locator);
+
+		$collection = new RouteCollection();
+		$routePath = __DIR__.'/../../Resources/config/routing/';
+		foreach (Yaml::parseFile($routePath.'all.yaml') as $yaml) {
+			$subCollection = $loader->load($routePath.$yaml['resource']);
+			if (isset($yaml['prefix'])) $subCollection->addPrefix($yaml['prefix']);
+			$collection->addCollection($subCollection);
+		}
+
+		$route = $collection->get($routeName);
+		$this->assertNotNull($route, sprintf('The route "%s" should exists', $routeName));
+		$this->assertSame($path, $route->getPath());
+		$this->assertSame($methods, $route->getMethods());
+	}
+
+	/**
+	 * @return array
+	 */
+	public function loadRoutingProvider()
+	{
+		return [
+			['login', '/login', ['GET', 'POST']],
+			['logout', '/logout', ['GET']],
+
+			['user_registration_register', '/registration/', ['GET', 'POST']],
+			['user_registration_check_email', '/registration/check-email', ['GET']],
+			['user_registration_confirm', '/registration/confirm/{token}', ['GET']],
+			['user_registration_confirmed', '/registration/confirmed', ['GET']],
+
+			['user_profile_show', '/profile/', ['GET']],
+			['user_profile_edit', '/profile/edit', ['GET', 'POST']],
+
+			['user_change_password', '/profile/change-password', ['GET', 'POST']],
+
+			['user_resetting_request', '/resetting/request', ['GET']],
+			['user_resetting_send_email', '/resetting/send-email', ['POST']],
+			['user_resetting_check_email', '/resetting/check-email', ['GET']],
+			['user_resetting_reset', '/resetting/reset/{token}', ['GET', 'POST']],
+		];
+	}
+}
