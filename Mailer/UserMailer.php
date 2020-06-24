cd48055b0f1867ce1c77a3445f19803bad29b533
Holecz Tamás <rasztamas79@gmail.com>
2020-06-24 15:37:36 +0200

    Fix mailer address

--- Mailer/UserMailer.php
+++ Mailer/UserMailer.php
@@ -19,19 +19,21 @@ class UserMailer implements UserMailerInterface
 	protected $translator;
 
 	protected $mailerAddress;
+	protected $mailerName;
 
 	public function __construct(MailerInterface $mailer, UrlGeneratorInterface $router, TranslatorInterface $translator, ParameterBagInterface $parameterBag)
 	{
 		$this->mailer = $mailer;
 		$this->router = $router;
 		$this->translator = $translator;
-		$this->mailerAddress = $parameterBag->get('mailer_address');
+		$this->mailerAddress = $parameterBag->has('mailer_sender') ? $parameterBag->get('mailer_sender') : 'test@test.com';
+		$this->mailerName = $parameterBag->has('mailer_sender_name') ? $parameterBag->get('mailer_sender_name') : '';
 	}
 
 	public function sendConfirmationEmailMessage(HTUserInterface $user): void
 	{
 		$email = (new TemplatedEmail())
-			->from(new Address($this->mailerAddress))
+			->from(new Address($this->mailerAddress, $this->mailerName))
 			->to(new Address($user->getEmail()))
 			->subject($this->translator->trans('Registration email subject', ['%user%' => $user], 'user'))
 			->htmlTemplate('@HTUser/registration_email.html.twig')
@@ -45,7 +47,7 @@ class UserMailer implements UserMailerInterface
 	public function sendResettingEmailMessage(HTUserInterface $user): void
 	{
 		$email = (new TemplatedEmail())
-			->from(new Address($this->mailerAddress))
+			->from(new Address($this->mailerAddress, $this->mailerName))
 			->to(new Address($user->getEmail()))
 			->subject($this->translator->trans('Resetting email subject', ['%user%' => $user], 'user'))
 			->htmlTemplate('@HTUser/resetting_email.html.twig')
